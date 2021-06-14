22dfb05aef8dd0932013ed94b3c65657d6be31a1
DESKTOP-FA5RKVS\Bismillah pc <s.m.abdulwassae@gnail.com>
2021-06-14 22:32:25 +0600

    initialize

---
+++ DemoApplication/processor/AmexPaymentProcessor.java
@@ -0,0 +1,8 @@
+package DemoApplication.processor;
+
+public class AmexPaymentProcessor implements IPaymentProcessor {
+    @Override
+    public void process() {
+        System.out.println("Amex is not implemented yet");
+    }
+}
